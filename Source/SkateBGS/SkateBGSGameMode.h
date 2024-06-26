7571cd0a0b6d4079b6bf2758abe10a932dde841a
Zhiro <luisf.gg1206@gmail.com>
2024-06-26 11:35:26 -0600

    Reupload files to server test 2

---
+++ Source/SkateBGS/SkateBGSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkateBGSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASkateBGSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASkateBGSGameMode();
+};
+
+
+
